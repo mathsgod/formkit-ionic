c090e8d8dc80adba237e9b1cebabdc08539a78df
Raymond Chong <mathsgod@yahoo.com>
2022-11-21 12:43:08 +0800

    dts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { IonicResolver } from 'unplugin-vue-components/resolvers'
-
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     Components({
       resolvers: [IonicResolver()]
+    }),
+    dts({
+      exclude: ["node_modules/**", "src/components/*.vue"],
     })
   ],
   build: {
